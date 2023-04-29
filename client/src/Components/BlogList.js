f4e37153e7ac1ac90e6f7c17dc4b3673355e73fb
Olivia <oliviatmanalastas@gmail.com>
2023-04-29 11:54:11 -0400

    Added code to blogList component

--- client/src/Components/BlogList.js
+++ client/src/Components/BlogList.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './BlogList.css'
 
-const BlogList = () => {
-   return(
-      <div>
+const BlogList = ({ data }) => {
+   const { blogSubject, blogBody } = data
 
+   return (
+      <div className="blogList">
+         <div className="blog-item">
+            <h1> {blogSubject} </h1>
+            <p> {blogBody} </p>
+         </div>
       </div>
    )
 }
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,12 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 import './Home.css'
 // This is where the blog title and body will be. Also the submit button
 const Home = () => {
    const [blogSubject, setBlogSubject] = useState("")
    const [blogBody, setBlogBody] = useState("")
+   const [blogPost, setBlogPost] = useState([])
+
+   // const link = 'http://localhost:3000/blogs'
+
+   useEffect(() => {
+      fetch("http://localhost:3000/blogs")
+      .then((res) => res.json())
+      .then((blog) => setBlogPost(blog))
+   }, [])
+
+   console.log(blogPost)
+
+   const handleAddPost = (newPost) => {
+      const updatedBlogPost = [...blogPost, newPost]
+      setBlogPost(updatedBlogPost)
+   }
 
    const handleBlogSubmit = ((e) => {
       e.preventDefault()
+      fetch("http://localhost:3000/blogs", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify({
+            blogSubject:blogSubject,
+            blogBody: blogBody
+         }),
+      })
+      .then((res) => res.json())
+      .then((newPost) => handleAddPost(newPost))
    })
 
    return (
@@ -36,7 +65,13 @@ const Home = () => {
                </span>
             </form>
          </div>
-         {/* I need an input field for the subject and body */}
+         <div>
+            {
+               blogPost.map((data) => {
+                  return <BlogList key={data.id} data={data}/>
+               })
+            }
+         </div>
       </div>
    )
 }
